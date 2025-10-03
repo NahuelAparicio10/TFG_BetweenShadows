7cc25204dcdaeb86c8742c7f5c79ba41de7c5141
hye <ye.hao@enti.cat>
2025-10-03 17:41:26 +0200

    Item Visual

--- BetweenShadows_UP/Assets/Code/_Scripts/Characters/Player/Inventory/PlayerInventory.cs
+++ BetweenShadows_UP/Assets/Code/_Scripts/Characters/Player/Inventory/PlayerInventory.cs
@@ -1,6 +1,65 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
 
-public class PlayerInventory : MonoBehaviour
+public sealed class PlayerInventory : MonoBehaviour
 {
+    public static PlayerInventory Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+            Configure();
+        }
+        else if (Instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    private VisualElement m_Root;
+    private VisualElement m_InventoryGrid;
+
+    private static Label m_ItemDetailHeader;
+    private static Label m_ItemDetailBody;
+    private static Label m_ItemDetailPrice;
+    private bool m_IsInventoryReady;
+    public static Dimensions SlotDimension { get; private set; }
+
+    private IEnumerator Start()
+    {
+        yield return null;
+        ConfigureSlotDimensions();
+        m_IsInventoryReady = true;
+    }
+
+    private async void Configure()
+    {
+        m_Root = GetComponentInChildren<UIDocument>().rootVisualElement;
+        m_InventoryGrid = m_Root.Q<VisualElement>("Grid");
+
+        VisualElement itemDetails = m_Root.Q<VisualElement>("ItemDetails");
+
+        m_ItemDetailHeader = itemDetails.Q<Label>("Header");
+        m_ItemDetailBody = itemDetails.Q<Label>("Body");
+        m_ItemDetailPrice = itemDetails.Q<Label>("SellPrice");
+    }
+
+    private void ConfigureSlotDimensions()
+    {
+        VisualElement firstSlot = m_InventoryGrid.Children().First();
+
+        SlotDimension = new Dimensions
+        {
+            Width = Mathf.RoundToInt(firstSlot.worldBound.width),
+            Height = Mathf.RoundToInt(firstSlot.worldBound.height)
+        };
+    }
+
 
 }
